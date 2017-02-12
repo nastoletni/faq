6de653e9023cd6f9ab27fa7bbd4a33393aed9112
Albert <w.albert221@gmail.com>
2017-02-12 13:14:16 +0100

    Moved .navbar to nastoletni/sass

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ const gulp = require('gulp'),
     csso = require('gulp-csso');
 
 gulp.task('default', () => {
-    return gulp.src(['sass/style.scss'])
+    return gulp.src(['node_modules/nastsoletni-sass/src/style.scss'])
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer())
         .pipe(csso())
