fba81c908c1718693b2c2b600ca7883baf010641
Albert <w.albert221@gmail.com>
2017-02-12 13:06:49 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# [faq.nastoletni.pl](https://faq.nastoletni.pl)
+
+Code is on MIT license, contents on CC-BY-SA 4.0.
+
+Możesz kontrybuować do strony poprzez utworzenie nowego Pull Requesta do repozytorium. Do każdego PRa dodaj notkę, dlaczego uważasz, że dodane przez Ciebie źródła są warte polecenia.
+
+W kodzie przestrzegaj semantyki.
---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+const gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    csso = require('gulp-csso');
+
+gulp.task('default', () => {
+    return gulp.src(['sass/style.scss'])
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(csso())
+        .pipe(gulp.dest('.'));
+});
